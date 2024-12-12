7b78f2e89e49a4e04c2655ced3dd1091d7472c07
JonasEngelin <jonas@essungaventilation.se>
2024-12-12 16:37:38 +0100

    Ship added as a triangle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+import player
+
 
 def main():
     print("Starting asteroids!")
@@ -11,17 +13,17 @@ def main():
     ticker = pygame.time.Clock()
     dt = 0
 
+    ship = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        color = (0,0,0)
+        color = (0, 0, 0)
         screen.fill(color)
+        ship.draw(screen)
         pygame.display.flip()
         dt = ticker.tick(60) / 1000
 
+
 if __name__ == "__main__":
     main()
-
-
